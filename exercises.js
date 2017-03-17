0589ba5467603ae54ba2e39b8e428fe47487ad24
bkam03 <bkam03@yahoo.com>
2017-03-16 14:58:21 -1000

    processed orders

--- exercises.js
+++ exercises.js
@@ -117,6 +117,7 @@ console.log("plainPerson",plainPerson);
 
 var completePerson = buildPerson(plainPerson, "bob", 43);
 console.log(completePerson,completePerson.name,completePerson.age);
+
 /*
    Display values of objects that are inside an array
    Declare a new variable named `arrayOfObjects` and set it to be [this array of objects
@@ -141,8 +142,69 @@ console.log(completePerson,completePerson.name,completePerson.age);
             =====
             ...
  */
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+]
 
 
+function printProcessedOrders(orders) {
+  for( var i = 0;i<orders.length;i++) {
+    console.log("id: ",orders[i].id);
+    console.log("purchase date: ",orders[i].date);
+    console.log("purchase total: ",orders[i].total);
+    console.log("=====");
+  }
+}
+printProcessedOrders(arrayOfObjects);
 /*
    Addition with an object
    Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`. 
